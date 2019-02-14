e59dadb7fd2ebf86506babffd63d624a951d47bf
mkamdom <mkamdom@cst-tp-b10115>
2019-02-14 16:51:08 +0100

    Implementation de la classe Platform

--- src/main/java/mooc/Platform.java
+++ src/main/java/mooc/Platform.java
@@ -3,6 +3,9 @@ package mooc;
 import java.util.*;
 
 public class Platform {
+    private Set<Course> listeCours;
+    private Set<Person> listeEtudiant;
+    private Set<Enrollment> listeInscrits;
 
 	public Platform() {
 	}
@@ -12,21 +15,24 @@ public class Platform {
 	 * @param c le cours à ajouter (non null)
 	 */
 	public void addCourse(Course c) {
-		throw new UnsupportedOperationException("Not supported yet.");
+            if (c == null){
+               throw new IllegalArgumentException ("la course ne peut pas etre nulle");
+            }
+		listeCours.add(c);
 	}
 
 	/**
 	 * @return les étudiants inscrits à l'université
 	 */
 	public Set<Person> students() {
-		throw new UnsupportedOperationException("Not supported yet.");
+        return listeEtudiant;
 	}
 
 	/**
 	 * @return les cours dispensés par l'université
 	 */
 	public Set<Course> courses() {
-		throw new UnsupportedOperationException("Not supported yet.");
+        return listeCours;
 	}
 
 	/**
@@ -34,7 +40,10 @@ public class Platform {
 	 * @param s  l'étudiant à inscrire (non null)
 	 */
 	public void registerStudent(Person s) {
-		throw new UnsupportedOperationException("Not supported yet.");
+            if (s == null){
+               throw new IllegalArgumentException ("il faut un etudiant");
+            }
+		listeEtudiant.add(s);
 	}
 
 	/**
@@ -44,8 +53,15 @@ public class Platform {
 	 * @throws PlatformException si l'étudiant n'est pas inscrit à l'université, 
 	 * ou si le cours n'est pas dispensé par l'université
 	 */
-	public void enroll(Person s, Course c) throws PlatformException {
-		throw new UnsupportedOperationException("Not supported yet.");
+	public void enroll(Person s, Course c) throws PlatformException { 
+              if (!listeEtudiant.contains(s) && listeCours.contains(c)){
+                  Enrollment inscription = new Enrollment(s, c);
+                  listeInscrits.add(inscription);
+                 }
+              else { 
+                 throw new PlatformException ("cet etudiant n'est pas inscrit à l'université", s, c);
+              }
+        
 	}
 
 	/**
@@ -55,7 +71,12 @@ public class Platform {
 	 * @throws PlatformException si l'étudiant a déjà une note àce cours
 	 */
 	public void withdraw(Person s, Course c) throws PlatformException {
-		throw new UnsupportedOperationException("Not supported yet.");
+            Enrollment inscription = new Enrollment(s, c);
+		if (inscription.studentHasMark()== true){
+                throw new PlatformException ("cet etudiant a deja une note à ce cours", s, c); }
+              else {
+                listeEtudiant.remove(s);
+                }
 	}
 
 	/**
@@ -68,7 +89,18 @@ public class Platform {
 	 * ou si l'étudiant a déjà une note pour ce cours
 	 */
 	public void setMark(Person s, Course c, int mark) throws PlatformException {
-		throw new UnsupportedOperationException("Not supported yet.");
+                Enrollment inscription = new Enrollment(s, c);
+		if (inscription.studentHasMark()== true){
+                throw new PlatformException ("cet etudiant a deja une note à ce cours", s, c); }
+                
+                if (!listeInscrits.contains(inscription)){
+                throw new PlatformException ("cet etudiant n'est pas inscrit", s, c); }
+                
+                if (!listeCours.contains(c)){
+                throw new PlatformException ("ce cours n'est pas dispensé par l'université", s, c); }
+                else {
+                    inscription.setMark(mark);
+                }    
 	}
 
 	/**
@@ -81,7 +113,18 @@ public class Platform {
 	 * ou si l'étudiant n'a pas encore de note à ce cours
 	 */
 	public int getMark(Person s, Course c) throws PlatformException {
-		throw new UnsupportedOperationException("Not supported yet.");
+		Enrollment inscription = new Enrollment(s, c);
+                if (!listeInscrits.contains(inscription)){
+                throw new PlatformException ("cet etudiant n'est pas inscrit", s, c); }
+                
+                if (!listeCours.contains(c)){
+                throw new PlatformException ("ce cours n'est pas dispensé par l'université", s, c); }
+                
+                if (inscription.studentHasMark()== false){
+                throw new PlatformException ("cet etudiant n'a pas encore de note à ce cours", s, c); }
+                else {
+                           return inscription.getMark();
+                    }
 	}
 
 	/**
@@ -90,7 +133,12 @@ public class Platform {
 	 * @throws PlatformException si le cours n'est pas dispensé par l'université
 	 */
 	public Set<Person> studentsForCourse(Course c) throws PlatformException {
-		throw new UnsupportedOperationException("Not supported yet.");
+               
+		if (!listeCours.contains(c)){
+                throw new PlatformException ("ce cours n'est pas dispensé par l'université", s, c); }
+                else {
+                    return 
+                }
 	}
 
 	/**
